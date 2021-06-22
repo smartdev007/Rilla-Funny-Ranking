635ce32b9bbd7f714cf2715f70cf63cc5eb7b987
smartdev007 <kevadiajenish7@gmail.com>
2021-06-22 19:34:31 -0400

    Add test cases for users

--- test/server.js
+++ test/server.js
@@ -21,5 +21,84 @@ describe("Get user info", () => {
 
 // Write more tests here if you wish to continue the testing in this file
 // -------- HERE ----------
+describe("Test user one and their rank", () => {
+  it("Should return username and their rank", (done) => {
+    chai
+      .request(server)
+      .get("/users")
+      .end((err, res) => {
+        res.should.have.status(200);
+        const userData = res.body[0]
+        
+        userData.user.should.equal("Jim Halpert");
+        userData.ranking.should.equal(1242);
+        done();
+      });
+  });
+});
+
+describe("Test user two and their rank", () => {
+  it("Should return username and their rank", (done) => {
+    chai
+      .request(server)
+      .get("/users")
+      .end((err, res) => {
+        res.should.have.status(200);
+        const userData = res.body[1]
+        
+        userData.user.should.equal("Dwight Schrute");
+        userData.ranking.should.equal(868);
+        done();
+      });
+  });
+});
+
+describe("Test user three and their rank", () => {
+  it("Should return username and their rank", (done) => {
+    chai
+      .request(server)
+      .get("/users")
+      .end((err, res) => {
+        res.should.have.status(200);
+        const userData = res.body[2]
+        
+        userData.user.should.equal("Stanley Hudson");
+        userData.ranking.should.equal(432);
+        done();
+      });
+  });
+});
+
+describe("Test user four and their rank", () => {
+  it("Should return username and their rank", (done) => {
+    chai
+      .request(server)
+      .get("/users")
+      .end((err, res) => {
+        res.should.have.status(200);
+        const userData = res.body[3]
+        
+        userData.user.should.equal("Phyllis Vance");
+        userData.ranking.should.equal(371);
+        done();
+      });
+  });
+});
+
+describe("Test user five and their rank", () => {
+  it("Should return username and their rank", (done) => {
+    chai
+      .request(server)
+      .get("/users")
+      .end((err, res) => {
+        res.should.have.status(200);
+        const userData = res.body[4]
+        
+        userData.user.should.equal("Pam Beasley");
+        userData.ranking.should.equal(1105);
+        done();
+      });
+  });
+});
 
 server.close();
